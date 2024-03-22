da663c5db52888105e97cab0bb2bdd4602c4091b
asadullah055 <asadullahahmed01@gmail.com>
2024-03-22 22:24:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+//body lib import
+const express = require('express')
+const app = express()
+const path = require('path')
+
+
+// security Middleware lib import
+const ratelimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongooseSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
+const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+// database import 
+
+const mongoose = require('mongoose')
+
+// security middleware implement
+app.use(cors())
+app.use(helmet())
+app.use(mongooseSanitize())
+app.use(xss())
+app.use(hpp())
+app.use(express.json())
+
+// request rateLimit
+const limiter = ratelimit({windowMs: 15 * 60 * 1000, max:3000})
+app.use(limiter)
+app.use(express.urlencoded({ extended: true }))
+
+// database connection
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => console.log("Database connection successfully"))
+  .catch((error => console.log(error)));
+
+  //frontend router define
+app.use(express.static('client/dist'))
+  app.get("*", function(req, res){
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  })
+
+  module.exports = app
